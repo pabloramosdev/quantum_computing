91d3d263d44df5311ab88d241a7ef43f124d5cae
Pablo Ramos <pabloramosdev@gmail.com>
2025-12-21 03:06:08 +0100

    definition of problem

---
+++ algorithms/problems/__init__.py
@@ -0,0 +1,27 @@
+from .problems import (
+    Problem, 
+    QAOAProblemMapping,
+    MaxCut,
+    MaxCliqueXMixer, 
+    MaxCliqueBitFlipMixer,
+    MinVertexCoverXMixer,
+    MinVertexCoverBitFlipMixer,
+    MaxWeightCycleXMixer,
+    MaxWeightCycleBitFlipMixer,
+    MaxIndependentSetXMixer,
+    MaxIndependentSetBitFlipMixer
+)
+
+__all__ = [ 
+    "Problem",
+    "QAOAProblemMapping",
+    "MaxCut",
+    "MaxCliqueXMixer",
+    "MaxCliqueBitFlipMixer",
+    "MinVertexCoverXMixer",
+    "MinVertexCoverBitFlipMixer",
+    "MaxWeightCycleXMixer",
+    "MaxWeightCycleBitFlipMixer",
+    "MaxIndependentSetXMixer",
+    "MaxIndependentSetBitFlipMixer"
+]
